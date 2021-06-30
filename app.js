490c9bf503021c7049c2ac649e31b8199a71eeac
MirajHoque <a.h.miraj2031@gmail.com>
2021-06-30 21:17:58 +0600

    conditional rendering
     v-if, v-show, v-else

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const app= Vue.createApp({
-  //data, methods
-  //template: '<h2>i am the template</h2>'
   data(){
     return {
+      showBooks: true,
       title: "The Final Empire",
       author: "Brandon Sanderson",
       age: 45
     }
   },
   methods:{
-    changeTitle(title){
-      //this.title= "words of radiance"; //this reference component itself
-      this.title= title;
+    toggleShowBooks(){
+      this.showBooks=!this.showBooks;
     }
+
   }
 });
 app.mount('#app');
