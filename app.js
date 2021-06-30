6d965e8dd752eed7a3170d26046ef938a9e8da1c
MirajHoque <a.h.miraj2031@gmail.com>
2021-07-01 02:24:28 +0600

    mouse event
    event object

--- app.js
+++ app.js
@@ -4,12 +4,27 @@ const app= Vue.createApp({
       showBooks: true,
       title: "The Final Empire",
       author: "Brandon Sanderson",
-      age: 45
+      age: 45,
+      x: 0,
+      y: 0
     }
   },
   methods:{
     toggleShowBooks(){
       this.showBooks=!this.showBooks;
+    },
+    handleEvent(e, data){ //e is an event obj
+      //whenever we have event that happen in the browser we can automaticall get access of event object.
+      console.log(e, e.type); //two parameter event, event type
+      //whenever we pass argument with event object, event obj must be first argument.
+      if(data){
+        console.log(data);
+      }
+    },
+    handleMouseMove(e){
+      this.x= e.offsetX;
+      this.y= e.offsetY;
+
     }
 
   }
