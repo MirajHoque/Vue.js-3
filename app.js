1ae2502416ee8da5691c35468a64e3ef668ad48b
MirajHoque <a.h.miraj2031@gmail.com>
2021-06-30 20:44:07 +0600

    cliek events

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const app= Vue.createApp({
       author: "Brandon Sanderson",
       age: 45
     }
+  },
+  methods:{
+    changeTitle(title){
+      //this.title= "words of radiance"; //this reference component itself
+      this.title= title;
+    }
   }
 });
 app.mount('#app');
