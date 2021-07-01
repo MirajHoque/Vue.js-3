46cb5546d02482064df598baf39aa30333b32528
MirajHoque <a.h.miraj2031@gmail.com>
2021-07-01 20:41:29 +0600

    outputing list
    v-for

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const app= Vue.createApp({
   data(){
     return {
       showBooks: true,
-      title: "The Final Empire",
-      author: "Brandon Sanderson",
-      age: 45,
-      x: 0,
-      y: 0
+      books: [
+        {title: 'author of the wind', author: 'patrik rothfuss'},
+        {title: 'the way of kings', author: 'brandon sanderson'},
+        {title: 'the final empire', author: 'brandon sanderson'}
+      ]
     }
   },
   methods:{
